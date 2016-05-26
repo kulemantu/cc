a0748cb8c294a50f85b795630c8fefc372f8f11b
Kago <kago.kagichiri@gmail.com>
2016-05-26 13:19:08 +0300

    Initial commit

---
+++ cli.py
@@ -0,0 +1,38 @@
+import socket
+import sys
+
+from src.cc import cc
+
+
+def client():
+    s = socket.socket()
+
+    host = cc.ask('What host shall we fetch the file from?', socket.gethostname())
+    port = cc.ask('What port on ' + host + ' shall we listen to?', 22222)
+
+    s.connect((host, port))
+
+    cc.say("Connected to " + host + ":" + str(port))
+
+    path = cc.persist('Save file as which path?')
+
+    with open(path, 'w+') as f:
+        cc.say("We've created the file " + path + " :)")
+        cc.say('Receiving data')
+        while True:
+            sys.stdout.write('.')
+            data = s.recv(1024)
+            if not data:
+                break
+
+            # Write data to a file
+            f.write(data)
+
+        f.close()
+        cc.say('We got the file!')
+
+    s.close()
+    cc.say('Connection to server closed!')
+
+
+client()
---
+++ srv.py
@@ -0,0 +1,48 @@
+import socket
+import sys
+from src.cc import cc
+
+
+def server():
+    s = socket.socket()
+
+    host = socket.gethostname()
+    port = cc.ask('What port on ' + host + ' shall we open?', 22222)
+
+    s.bind((host, port))
+    s.listen(5)
+
+    print("Listening on " + host + ":" + str(port))
+
+    path = cc.persist('What file shall we serve?')
+
+
+    while True:
+        c, addr = s.accept()
+        print('Got connection from', addr)
+
+        # Re-open the file when the client connects
+        f = open(path, 'r')
+        l = f.read(1024)
+        print('Opened file for sending')
+        print('Sending file over')
+
+        while l:
+            sys.stdout.write('.')
+            c.send(l)
+            l = f.read(1024)
+
+        c.close()
+        print('Kicked the client!')
+
+        f.close()
+        print('We sent the file ' + path + '!')
+
+    s.close()
+    print('Connection to server closed!')
+
+try:
+    server()
+except KeyboardInterrupt:
+    print('Goodbye!')
+    exit(1)
